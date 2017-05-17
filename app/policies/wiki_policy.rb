b3dc4ec2b9e0c7f6f05b0ed30dee49d8d52179b0
Behic Akgun <behica@gmail.com>
2017-05-17 17:38:04 +0000

    Updated policies for user roles.

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,26 +1,4 @@
 class WikiPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      scope
-    end
-  end
-  
-  def update?
-    user.present?
-  end
-  
-  def new?
-    user.present?
-  end
-  
-  def create?
-    user.present?
-  end
-  
-  def edit?
-    user.present?
-  end
-  
   def destroy?
     user.role == 'admin'
   end
