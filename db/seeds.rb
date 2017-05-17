2f95c760795de8435ff796e8cbb711e3b4c731dc
Behic Akgun <behica@gmail.com>
2017-05-17 18:13:01 +0000

    Added Faker and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,25 @@ require 'random_data'
 
 50.times do
   Wiki.create!(
-    title:    RandomData.random_sentence,
-    body:     RandomData.random_paragraph,
+    title:    Faker::Lorem.sentence,
+    body:     Faker::Lorem.paragraph,
     private:  false
   )
 end
 
 admin = User.create!(
-  name:     'Admin User',
   email:    'admin@example.com',
   password: 'helloworld',
-  password_confirmation: 'helloworld',
-  confirm: true
+  password_confirmation: 'helloworld'
 )
+
+10.times do
+  User.create!(
+  email:    Faker::Internet.unique.email,
+  password: Faker::Lorem.characters(6..10)
+)
+end
 
 puts "Seed finished"
 puts "#{Wiki.count} wikis created"
+puts "#{User.count} users created"
